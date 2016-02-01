8f33dc68a0881d7167c29d78e519e705033fa3e3
Brett Harrison <brett.harrison@musicmastermind.com>
2016-02-01 10:18:48 -0800

    clean up

--- Android.Digits/Additions/Com.Digits.Sdk.Android.ContactsCallback.cs
+++ Android.Digits/Additions/Com.Digits.Sdk.Android.ContactsCallback.cs
@@ -6,14 +6,7 @@ namespace Com.Digits.Sdk.Android
 	public partial class ContactsCallback : Com.Twitter.Sdk.Android.Core.Callback
 	{
 		public override abstract void Success (Result p0);
-		//{
-			//_Success(p0);
-	//	}
-
 		public override abstract void Failure (TwitterException p0);
-		//{
-			//_Failure(p0);
-		//}
 	}
 }
 
