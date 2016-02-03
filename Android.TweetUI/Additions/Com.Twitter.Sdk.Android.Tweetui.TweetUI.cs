39132b8f5295cc2817622e935a0b94ba029e7033
Brett Harrison <brett.harrison@musicmastermind.com>
2016-02-03 11:08:45 -0800

    tweetui

---
+++ Android.TweetUI/Additions/Com.Twitter.Sdk.Android.Tweetui.TweetUI.cs
@@ -0,0 +1,10 @@
+﻿namespace Com.Twitter.Sdk.Android.Tweetui
+{
+	public partial class TweetUi : global::IO.Fabric.Sdk.Android.Kit
+	{
+		public override int CompareTo(global::Java.Lang.Object p0)
+		{
+			return CompareTo ((Com.Twitter.Sdk.Android.Tweetui.TweetUi) p0);
+		}
+	}
+}
