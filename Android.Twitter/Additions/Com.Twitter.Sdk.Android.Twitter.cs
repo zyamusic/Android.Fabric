65532ce6f667af98de0d7bf64d9d58be7c1e65f9
Brett Harrison <brett.harrison@musicmastermind.com>
2016-02-02 18:20:42 -0800

    adding twitterkit

---
+++ Android.Twitter/Additions/Com.Twitter.Sdk.Android.Twitter.cs
@@ -0,0 +1,10 @@
+﻿namespace Com.Twitter.Sdk.Android
+{
+	public partial class Twitter : global::IO.Fabric.Sdk.Android.Kit
+	{
+		public override int CompareTo(global::Java.Lang.Object p0)
+		{
+			return CompareTo ((Com.Twitter.Sdk.Android.Twitter) p0);
+		}
+	}
+}
