949d843310a37702b29941510cd76b0d5d5d1243
Brett Harrison <brett.harrison@musicmastermind.com>
2016-02-03 11:33:51 -0800

    tweet-composer

---
+++ Android.TweetComposer/Additions/Com.Twitter.Sdk.Android.Tweetcomposer.Tweetcomposer.cs
@@ -0,0 +1,11 @@
+﻿namespace Com.Twitter.Sdk.Android.Tweetcomposer
+{
+	public partial class TweetComposer : global::IO.Fabric.Sdk.Android.Kit
+	{
+		public override int CompareTo(global::Java.Lang.Object p0)
+		{
+			return CompareTo ((Com.Twitter.Sdk.Android.Tweetcomposer.TweetComposer) p0);
+		}
+	}
+}
+
